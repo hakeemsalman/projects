147c82be6c73c58a9ed90ec74d3f9390bda14997
hakeemsalman <salman74163@gmail.com>
2023-12-15 22:13:44 +0530

    updated

---
+++ cms-dashboard/src/pages/users/Users.jsx
@@ -0,0 +1,15 @@
+import DataTable from '../../components/dataTable/DataTable';
+import './users.scss';
+const Users = () => {
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add new User</button>
+      </div>
+      <DataTable />
+    </div>
+  )
+}
+
+export default Users
