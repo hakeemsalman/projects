8ab13a8d38a0e55fa2a24fcc2185ee83acd01168
hakeemsalman <salman74163@gmail.com>
2024-02-23 14:24:40 +0530

    updated

---
+++ salesforce-extension/popup.js
@@ -0,0 +1,8 @@
+// popup.js
+document.addEventListener('DOMContentLoaded', function () {
+    var simulateButton = document.getElementById('simulateMobile');
+
+    simulateButton.addEventListener('click', function () {
+        // TODO: Add logic to simulate mobile login
+    });
+});
